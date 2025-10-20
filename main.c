21aec684b700d59b5e4391dad79a36e32294da7c
aaron28154158 <147162239+aaron28154158@users.noreply.github.com>
2025-10-20 22:29:06 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#define PERIPHERAL_BASE			(0x40000000UL)
+#define AHB1_PERIPHERAL_OFFSET	(0x00020000UL)
+#define AHB1_PERIPHERAL_BASE	(PERIPHERAL_BASE + AHB1_PERIPHERAL_OFFSET)
+
+#define GPIOA_OFFSET			(0x0000)
+#define GPIOA_BASE				(AHB1_PERIPHERAL_BASE + GPIOA_OFFSET)
+
+#define RCC_OFFSET				(0x3800UL)
+#define RCC_BASE				(AHB1_PERIPHERAL_BASE + RCC_OFFSET)
+
+#define RCC_AHB1ENR_OFFSET		(0x30UL)
+#define RCC_AHB1ENR_BASE		(* (volatile unsigned int *) (RCC_BASE + RCC_AHB1ENR_OFFSET))
+#define RCC_AHB1ENR_REGISTER	RCC_AHB1ENR_BASE
+
+#define GPIO_MODER_OFFSET		(0x00UL)
+#define GPIOA_MODER_BASE		(* (volatile unsigned int *) (GPIOA_BASE + GPIO_MODER_OFFSET))
+#define GPIOA_MODER_REGISTER	GPIOA_MODER_BASE
+
+#define MODER_OUTPUT_MASK		(0x01UL)
+#define MODER5_POS				(10)
+
+#define GPIO_ODR_OFFSET			(0x14UL)
+#define GPIOA_ODR_BASE			(* (volatile unsigned int *) (GPIOA_BASE + GPIO_ODR_OFFSET))
+#define	GPIOA_ODR_REGISTER		GPIOA_ODR_BASE
+
+#define GPIOAEN					(1UL << 0)
+
+#define PIN5					(1UL << 5)
+#define LED_PIN					PIN5
+
+int main(void)
+{
+	/* Enable clock access to GPIOA */
+	RCC_AHB1ENR_REGISTER |= GPIOAEN;
+
+	/* Set PA5 as output pin */
+	GPIOA_MODER_REGISTER |= (MODER_OUTPUT_MASK << MODER5_POS);
+
+	while(1)
+	{
+		GPIOA_ODR_REGISTER |= LED_PIN;
+	}
+}
+
